efb34b9f4d39198c5086a94189ec8fcca77dcb17
JacoboCuartas <jacobocuartas09@gmail.com>
2025-11-25 23:02:55 -0500

    Avance graficas

--- Graficas.py
+++ Graficas.py
@@ -1,14 +1,37 @@
 import tkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 def Visualizar():
     New_Reg = tk.Toplevel()
     New_Reg.title("Graficas")
-    New_Reg.geometry("600x200")
+    New_Reg.geometry("600x600")
+    New_Reg.resizable(False, False)
 
     def volver():
         New_Reg.destroy()
 
+    # Crear la figura y los ejes de Matplotlib
+    fig = Figure(figsize=(5, 4), dpi=100)
+    ax = fig.add_subplot(111) # Crea un único subplot
+
+    # Generar y dibujar la gráfica (ejemplo con una función seno)
+    x = [i * 0.1 for i in range(100)]
+    y = [i * 0.1 for i in range(100)]
+    ax.plot(x, y)
+    ax.set_title("Gráfico de línea de ejemplo")
+
+    # Integrar el gráfico en Tkinter
+    canvas = FigureCanvasTkAgg(fig, master=frame_grafica)
+    canvas_widget = canvas.get_tk_widget()
+
     text1 = tk.Label(New_Reg, text="Seleccione una estadistica para visualizar")
+
+    # === Crear un frame para el gráfico ===
+    frame_grafica = tk.Frame(New_Reg)
+    frame_grafica.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+    canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
     btn1 = tk.Button(New_Reg, text="Grafica 1", width=20, height=3)
     btn2 = tk.Button(New_Reg, text="Grafica 2", width=20, height=3)
